0f19fd75d1a556f5ea0f31fef9e5f0017dbfb440
jason.huang <hj74535099@gmail.com>
2026-03-12 00:18:05 +0800

    feat: 更新工具函数导出 (__init__.py)

--- src/backend/app/utils/__init__.py
+++ src/backend/app/utils/__init__.py
@@ -1,3 +1,41 @@
 """
-Utils module
+工具函数导出
 """
+from app.utils.password import (
+    hash_password,
+    verify_password,
+    get_password_hash,
+)
+from app.utils.jwt import (
+    create_access_token,
+    verify_token,
+    decode_token,
+)
+from app.utils.validator import (
+    validate_phone,
+    validate_email,
+    validate_password,
+    validate_verify_code,
+    sanitize_string,
+    validate_date_format,
+    validate_indicator_value,
+)
+
+__all__ = [
+    # Password
+    "hash_password",
+    "verify_password",
+    "get_password_hash",
+    # JWT
+    "create_access_token",
+    "verify_token",
+    "decode_token",
+    # Validator
+    "validate_phone",
+    "validate_email",
+    "validate_password",
+    "validate_verify_code",
+    "sanitize_string",
+    "validate_date_format",
+    "validate_indicator_value",
+]
